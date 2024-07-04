696c5ee98d9a8598b49d2bc4cca0848c2a1d1176
Olga <olgahrytsiuk777@gmail.com>
2024-07-04 22:02:55 +0200

    solution

--- movie/models.py
+++ movie/models.py
@@ -5,9 +5,6 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250, null=True, blank=True)
     duration = models.IntegerField()
-    #
-    # class Meta:
-    #     verbose_name_plural = "movies"
 
     def __str__(self):
         return f" Movie: {self.title} (id = {self.id})"
